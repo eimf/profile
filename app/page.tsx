fdcfdbaaad617cbf93583ca6d997ed958d516f7f
eimf <lzdzel@gmail.com>
2023-11-25 21:47:06 -0600

    sidebar and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
-import Image from "next/image";
+
+import SideNav from './sidebar/sidebar'
 import { lusitana } from "@/app/ui/fonts";
 
 export default function Home() {
   return (
     <main
-      className={`${lusitana.className} flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`${lusitana.className} flex min-h-screen flex-col items-left justify-between p-24`}
     >
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          // tailwindcss: width: an small image at the top most left corner of the page
-          // tailwindcss: dynamic width: 1/4 of the screen
-          className="w-1/6 h-1/6 rounded-full mr-4 float-left"
-          src="/photo.jpeg"
-          alt="Ezequiel Lopez"
-          width={200}
-          height={200}
-        />
+        {/* divide the whole screen in 2 colums for large and medium screen and a single one for small screens*/}
+        <div className="flex flex-col items-center justify-between">
+        </div>
+        <div className="right-section">
+
+        </div>
+        
+        <div className="image-container">
+          
+        </div>
       </div>
     </main>
   );
---
+++ app/sidebar/sidebar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import NavLinks from "./nav-links";
+import Image from "next/image";
+import { PowerIcon } from "@heroicons/react/24/outline";
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div className="w-32 text-white md:w-40">
+          <h1 className="text-4xl font-bold">Ezequiel Lopez</h1>
+
+          <h2 className="text-2xl font-bold">professional software engineer</h2>
+          <Image
+            className="w-1/2 h-1/2 rounded-full"
+            src="/photo.jpeg"
+            alt="Ezequiel Lopez"
+            width={200}
+            height={200}
+          />
+        </div>
+      </Link>
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
